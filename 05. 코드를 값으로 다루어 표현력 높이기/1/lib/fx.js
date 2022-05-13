d99c37608641e6f026d28d09ef3aab6ac886d013
yujeong-choung <yujeong.choung@gmail.com>
2022-05-13 18:45:26 +0900

    5. 코드를 값으로 다루어 표현력 높이기 완료

--- 05. 코드를 값으로 다루어 표현력 높이기/1/lib/fx.js
+++ 05. 코드를 값으로 다루어 표현력 높이기/1/lib/fx.js
@@ -2,7 +2,12 @@ const log = console.log;
 
 const curry = f =>
   (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
+// f에서 사용할 인자를 (a, ..._)받고
+// 두개이상 인자가 있을 때 : _.length를 의미
+//  f(a, ..._) :  받아 둔 함수 즉시 실행
+// 아니라면 다시 한번 함수를 리턴
 
+// 인자를 하나만 받으면 이후 인자를 받기로 기다리는 함수를 리턴
 const map = curry((f, iter) => {
   let res = [];
   for (const a of iter) {
